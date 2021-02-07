01d341c12473e82aaebbbd999dfba904d4be9c26
Patricia Camiansky <patcky@hotmail.com>
2021-02-07 18:04:59 -0300

    Added routes, devise, created areas model and controller, added some views

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
+gem 'devise'
 
 group :development, :test do
   gem 'pry-byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :areas, only: %i[create, index, show, update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
