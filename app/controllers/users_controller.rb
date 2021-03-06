3fecadb7a9d8675840ef7262baeb7b349001da5f
Angelita Santos <angelitasantos@gmail.com>
2021-03-06 09:27:45 -0300

    included method update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
+
+    def update
+        @user = current_user
+    
+        if @user.update_attributes(current_user_params)
+          flash[:notice] = "Cadastro atualizado com sucesso."
+        else
+          flash[:alert] = "Algo deu errado. Não foi possível alterar o cadastro."
+        end
+        redirect_to page_path
+    
+      end
+    
+      private
+      
+      def current_user_params
+        params.require(:user).permit(:name, :district, :city, :state, :status)
+      end
+
 end
