1cb120bd9a3c3433bd9aa64af828854797541722
Matheus Ligabue <matheus.legabue@ehi.com>
2021-05-02 14:45:38 -0300

    Add tailwind config and remove bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,8 +22,6 @@ require("channels")
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
 
-// External imports
-import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -32,3 +30,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+import "stylesheets/application"
