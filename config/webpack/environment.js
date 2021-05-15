c958bfc04d813c80aeb4b0887f6e6f1641b575c7
Patricia Camiansky <52200149+patcky@users.noreply.github.com>
2021-05-15 01:38:25 -0300

    Revert "Add tailwind config and remove bootstrap"

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,17 @@
-const { environment } = require("@rails/webpacker");
+const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack');
 
 // Preventing Babel from transpiling NodeModules packages
-environment.loaders.delete("nodeModules");
+environment.loaders.delete('nodeModules');
 
-module.exports = environment;
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+);
+
+module.exports = environment
