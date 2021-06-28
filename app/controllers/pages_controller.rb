2f5e19c8a15e50d4921996e58e7d409436de4697
Matheus Ligabue <matheus.legabue@ehi.com>
2021-06-28 01:44:03 -0300

    Add view arrays from controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
+
+
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    cause = Struct.new(:name, :description, :image, :image_alt);
+    @causes = [
+      cause.new("LGBTQIA+", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."),
+      cause.new("Gênero", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."),
+      cause.new("Etnia", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.")
+    ]
+
+    brand = Struct.new(:name, :image, :image_alt);
+    @brands = (1..9).map { |i| brand.new("Marca #{i}") };
   end
 
   def about
