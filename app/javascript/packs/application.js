5e6233d57ddf16baf73dd2e36f024e492690a4aa
Patricia Camiansky <patcky@hotmail.com>
2021-08-19 18:08:04 -0300

    Wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,7 +23,6 @@ require("channels")
 // ----------------------------------------------------
 
 // External imports
-import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
