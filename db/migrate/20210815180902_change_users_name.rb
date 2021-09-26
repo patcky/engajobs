adebb435f14d1d7fa9634f8907c558b29f63ba01
Patricia Camiansky <patcky@hotmail.com>
2021-09-26 14:10:40 -0300

    Fix bug

--- db/migrate/20210815180902_change_users_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUsersName < ActiveRecord::Migration[6.1]
-  def change
-    change_column :users, :name, :string, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -15,34 +15,6 @@ ActiveRecord::Schema.define(version: 2021_09_26_121907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
   create_table "address_business_hours", force: :cascade do |t|
     t.integer "day", null: false
     t.time "open_time", null: false
@@ -187,7 +159,7 @@ ActiveRecord::Schema.define(version: 2021_09_26_121907) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name", null: false
+    t.string "name"
     t.string "district"
     t.string "city"
     t.string "state"
@@ -198,8 +170,6 @@ ActiveRecord::Schema.define(version: 2021_09_26_121907) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "address_business_hours", "addresses"
   add_foreign_key "address_contacts", "addresses"
   add_foreign_key "addresses", "public_profiles"
