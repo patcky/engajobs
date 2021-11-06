9e9de189da0271bcadb3c8ddf1979a7830edd3dd
elenicehalma <elenice.halma@gmail.com>
2021-11-05 21:55:47 -0400

    Navbar responsive created.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,16 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Navbar
+  let show = true;
+  const menuSection = document.querySelector(".menu-section")
+  const navbarButton = menuSection.querySelector(".navbar_button")
+
+  navbarButton.addEventListener("click", () => {
+    menuSection.classList.toggle("on", show)
+    show = !show;
+  })
+});
