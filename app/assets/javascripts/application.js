646740a2b68bcb862ecb27db72fc2113ad371e2d
Patricia Camiansky <patcky@hotmail.com>
2021-11-07 14:46:00 -0300

    Squashed commit of the following:
    commit 7eda9e17c908b1d8820e159688a72144e88c681c
    Merge: 1ec62f2 9e9de18
    Author: Patricia Camiansky <52200149+patcky@users.noreply.github.com>
    Date:   Sun Nov 7 14:36:34 2021 -0300
    
        Merge pull request #33 from patcky/navbar
    
        Navbar responsive created.
    
    commit 9e9de189da0271bcadb3c8ddf1979a7830edd3dd
    Author: elenicehalma <elenice.halma@gmail.com>
    Date:   Fri Nov 5 21:55:47 2021 -0400
    
        Navbar responsive created.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,16 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-//= require algolia/v3/algoliasearch.min
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Navbar
+  let show = true;
+  const menuSection = document.querySelector(".menu-section")
+  const navbarButton = menuSection.querySelector(".navbar_button")
+
+  navbarButton.addEventListener("click", () => {
+    menuSection.classList.toggle("on", show)
+    show = !show;
+  })
+});
